42516cbcb886fec653da036d4643ae42dce409eb
Franko <francodonadio2005@gmail.com>
2023-10-03 13:18:26 -0300

    intento pase archivos

--- 7.7.c
+++ 7.7.c
@@ -10,29 +10,57 @@ struct guarda
 
 void carga(struct guarda *dato)
 {
-    printf("Ingrese el legajo del estudiante  :");
-    scanf(" %ld", &dato->legajo);
-    printf("Ingrese el nombre del estudiante  :");
-    scanf("%s", &dato->nombre);
-    printf("Ingrese el apellido del estudiante:");
-    scanf("%s", &dato->apellido);
+    FILE *fp;
+    fp = open("archivo.txt", "r");
+    const char s[2] = ','
+    char *token;
+    char archivo[31];
+
+    while(fp!=NULL){
+        archivo[i]=fgetc(fp);
+        i++
+        if (feof(fp))
+        {
+            break;
+        }
+    }
+    token = strtok(archivo,s);
+    while(token!=NULL){
+        token = strtok(NULL,s);
+    }
+    return 0;
+}
+
+int fin(struct guarda *dato, int i)
+{
+    if (dato[i - 1].legajo != 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
 
 int main()
 {
-    struct guarda *datos;
+    struct guarda *datos = NULL;
     int i = 0;
-    datos = malloc(sizeof(struct guarda));
+    int flag = 0;
+    datos = realloc(datos, (i + 1) * sizeof(struct guarda));
     do
     {
         carga(&datos[i]);
+        flag = fin(&datos, i);
         i++;
-    } while (datos[i - 1].legajo != 0);
+    } while (flag == 0);
     for (int j = 0; j < i - 1; j++)
     {
         printf("Legajo: %ld\n", datos[j].legajo);
         printf("Nombre: %s\n", datos[j].nombre);
         printf("Apellido: %s\n", datos[j].apellido);
     }
+    free(datos);
     return 0;
 }
