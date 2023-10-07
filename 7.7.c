767e709f9f564a4f7cb9a4ef291d62d5efdd4df3
Franko <francodonadio2005@gmail.com>
2023-10-07 10:27:12 -0300

    entendi mal la consigna el trues es el posta

--- 7.7.c
+++ 7.7.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 
 struct guarda
 {
@@ -11,24 +16,35 @@ struct guarda
 void carga(struct guarda *dato)
 {
     FILE *fp;
-    fp = open("archivo.txt", "r");
-    const char s[2] = ','
+    struct stat fpstat;
+    fp = fopen("archivo.txt", "r");
+    stat("archivo.txt", &fpstat);
+    const char s[2] = ",";
     char *token;
-    char archivo[31];
-
-    while(fp!=NULL){
-        archivo[i]=fgetc(fp);
-        i++
+    char *archivo;
+    int i = 0;
+    int j = 1;
+    int x = 0;
+    dato = (struct guarda *) malloc(sizeof(struct guarda));
+    archivo = (char *)malloc((size_t)fpstat.st_size);
+    while (fp != NULL)
+    {
+        archivo[i] = fgetc(fp);
+        i++;
         if (feof(fp))
         {
             break;
         }
     }
-    token = strtok(archivo,s);
-    while(token!=NULL){
-        token = strtok(NULL,s);
+    token = strtok(archivo, s);
+    while (token != NULL)
+    {
+        dato = (struct guarda *) realloc(dato, sizeof(struct guarda)*(j + 1));
+        strcpy((dato + j)->nombre, token);
+        token = strtok(NULL, s);
+        printf("%s\n", (dato + j)->nombre);
+        j++;
     }
-    return 0;
 }
 
 int fin(struct guarda *dato, int i)
@@ -51,8 +67,8 @@ int main()
     datos = realloc(datos, (i + 1) * sizeof(struct guarda));
     do
     {
-        carga(&datos[i]);
-        flag = fin(&datos, i);
+        carga(datos);
+        flag = fin(datos, i);
         i++;
     } while (flag == 0);
     for (int j = 0; j < i - 1; j++)
